b5a455dea1268fc00620873c87245612146e451d
Richard Sando <richardsando@Richards-MBP.modem>
2021-11-23 12:13:58 +1100

    added search component

--- api/server.js
+++ api/server.js
@@ -1,26 +1,22 @@
-// importing the dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const { application } = require('express');
-const axios = require('axios');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const axios = require("axios");
+
+require("dotenv").config();
 
 const apikey = process.env.MUSEUMS_VIC_API_KEY;
-
 const app = express();
 
 app.use(helmet());
 
 app.use(cors());
 
-// adding morgan to log HTTP requests
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-// defining an endpoint to return all ads
-app.get('/articles', async (req, res) => {
+app.get("/articles", async (req, res) => {
   try {
     const response = await axios.get(
       `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/articles`,
@@ -38,7 +34,7 @@ app.get('/articles', async (req, res) => {
   }
 });
 
-app.get('/article', async (req, res) => {
+app.get("/article", async (req, res) => {
   const { id } = req.headers;
   try {
     const response = await axios.get(
@@ -57,147 +53,166 @@ app.get('/article', async (req, res) => {
   }
 });
 
-app.get('/item', async (req, res) => {
-    const { id } = req.headers;
-    try {
-      const response = await axios.get(
-        `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/${id}`,
-        {
-          headers: {
-            apikey: apikey,
-          },
-        }
-      );
-      const data = response.data;
-      res.send(data);
-    } catch (err) {
-      console.log(err);
-      res.status(500).send(err);
-    }
-  });
+app.get("/item", async (req, res) => {
+  const { id } = req.headers;
+  try {
+    const response = await axios.get(
+      `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/${id}`,
+      {
+        headers: {
+          apikey: apikey,
+        },
+      }
+    );
+    const data = response.data;
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 
-  app.get('/items', async (req, res) => {
-    try {
-      const response = await axios.get(
-        `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/items`,
-        {
-          headers: {
-            apikey: apikey,
-          },
-        }
-      );
-      const data = response.data;
-      res.send(data);
-    } catch (err) {
-      console.log(err);
-      res.status(500).send(err);
-    }
-  });
+app.get("/items", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/items`,
+      {
+        headers: {
+          apikey: apikey,
+        },
+      }
+    );
+    const data = response.data;
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 
-  app.get('/specie', async (req, res) => {
-    const { id } = req.headers;
-    try {
-      const response = await axios.get(
-        `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/${id}`,
-        {
-          headers: {
-            apikey: apikey,
-          },
-        }
-      );
-      const data = response.data;
-      res.send(data);
-    } catch (err) {
-      console.log(err);
-      res.status(500).send(err);
-    }
-  });
+app.get("/specie", async (req, res) => {
+  const { id } = req.headers;
+  try {
+    const response = await axios.get(
+      `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/${id}`,
+      {
+        headers: {
+          apikey: apikey,
+        },
+      }
+    );
+    const data = response.data;
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 
-  app.get('/species', async (req, res) => {
-    try {
-      const response = await axios.get(
-        `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/species`,
-        {
-          headers: {
-            apikey: apikey,
-          },
-        }
-      );
-      const data = response.data;
-      res.send(data);
-    } catch (err) {
-      console.log(err);
-      res.status(500).send(err);
-    }
-  });
+app.get("/species", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/species`,
+      {
+        headers: {
+          apikey: apikey,
+        },
+      }
+    );
+    const data = response.data;
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 
-  app.get('/specimen', async (req, res) => {
-    const { id } = req.headers;
-    try {
-      const response = await axios.get(
-        `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/${id}`,
-        {
-          headers: {
-            apikey: apikey,
-          },
-        }
-      );
-      const data = response.data;
-      res.send(data);
-    } catch (err) {
-      console.log(err);
-      res.status(500).send(err);
-    }
-  });
+app.get("/specimen", async (req, res) => {
+  const { id } = req.headers;
+  console.log(id);
+  try {
+    const response = await axios.get(
+      `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/${id}`,
+      {
+        headers: {
+          apikey: apikey,
+        },
+      }
+    );
+    const data = response.data;
+    console.log(data);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 
-  app.get('/specimens', async (req, res) => {
-    try {
-      const response = await axios.get(
-        `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/specimens`,
-        {
-          headers: {
-            apikey: apikey,
-          },
-        }
-      );
-      const data = response.data;
-      res.send(data);
-    } catch (err) {
-      console.log(err);
-      res.status(500).send(err);
-    }
-  });
+app.get("/specimens", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/specimens`,
+      {
+        headers: {
+          apikey: apikey,
+        },
+      }
+    );
+    const data = response.data;
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 
-  app.get('/search', async (req, res) => {
-    try {
-      const response = await axios.get(
-        `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/search`,
-        {
-          headers: {
-            apikey: apikey,
-          },
-        }
-      );
-      const data = response.data;
-      res.send(data);
-    } catch (err) {
-      console.log(err);
-      res.status(500).send(err);
-    }
-  });
-  
-  
+app.get("/search", async (req, res) => {
+  try {
+    // console.log(req.query.query);
+    const query = req.query.query;
+    const recordtype = req.query.recordtype
+    // const {query, recordtype} = req.query
+    console.log(req.query)
+    const response = await axios.get(
+      "https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/search",
+      {
+        params: {
+          query,
+          recordtype
+        },
+        headers: {
+          apikey: apikey,
+        },
+      }
+    );
+    const data = response.data
+    res.send(data);
+  } catch (error) {
+    res.send(error);
+    console.log(error);
+  }
+});
 
-
-  
-  
-
-  
-  
-
-
+// app.post("/search", (req, res) => {
+//   try {
+//     console.log(req.body)
+// const response = await axios.get(
+//   `https://wovg-community.gateway.prod.api.vic.gov.au/museumvictoria/v1.0/collections/search`,
+//   {
+//     headers: {
+//       apikey: apikey,
+//     },
+//   }
+// );
+// const data = response.data;
+//     res.send("yo");
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send(err);
+//   }
+// });
 
 // starting the server
 app.listen(3001, () => {
-  console.log('listening on port 3001');
+  console.log("listening on port 3001");
 });
